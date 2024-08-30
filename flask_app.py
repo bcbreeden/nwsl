c865f8e699729a08185822b009405f98c704c9ff
Brian Breeden <bcbreeden@proton.me>
2024-08-30 19:25:09 -0400

    basic boiler plate for flask app

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from db import (db_games_xgoals, db_games, db_goalkeeper_goals_added,db_goalkeeper_xgoals,
+                db_player_goals_added, db_player_info, db_player_xgoals, db_player_xpass,
+                db_setup, db_team_goals_added, db_team_info, db_team_xgoals, db_team_xpass)
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+'''
+Renders the index template.
+'''
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
